69d0a20c1ca55452a823dfdc03fb329ab423b727
Sofhia Chromiec <sofhia.chromiec@pucpr.edu.br>
2025-09-14 03:17:33 -0300

    modal de adicionar item no checklist funcionando

--- WeCheck/views/checklist_view.php
+++ WeCheck/views/checklist_view.php
@@ -21,7 +21,7 @@ $itens = ChecklistController::listarItens($idAuditoria);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/modal_checklist.css">
+    <link rel="stylesheet" href="../assets/css/modal_criacaochecklist.css">
     <link rel="stylesheet" href="../assets/css/checklist_criacao.css">
     <title>WeCheck</title>
     
