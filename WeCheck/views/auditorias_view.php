6159f387e5f7859af6e1a0c8d636ef0cf67f296c
Sofhia Chromiec <sofhia.chromiec@pucpr.edu.br>
2025-09-14 03:09:12 -0300

    rota correta

--- WeCheck/views/auditorias_view.php
+++ WeCheck/views/auditorias_view.php
@@ -21,7 +21,6 @@ if (!$idUsuario) {
             <img src="../assets/logo/WeCheck_Escrita.png" alt="Nome WeCheck">
         </div>
         <div class="botoes-nav">
-            <a id="inicio" href="index.php?rota=cadastro">Início</a> 
             <a id="conta" href="#">Conta</a>
         </div>
     </header>
